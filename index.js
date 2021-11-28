462f1f429cadad0d90556fb7925cd2a64ce6536a
laminarmodes <anya.traille@gmail.com>
2021-11-28 12:03:07 +0800

    Added package.json

---
+++ index.js
@@ -0,0 +1,3 @@
+// Used npm init to initialize project with package.json
+// Ran npm install --save express to install express module
+// Ran npm install --save body-parser to install body-parser module
